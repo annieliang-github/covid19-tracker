fb063a9bbd4f6b3b226a3a8533a3896e674407ea
annieliang-github <52332955+annieliang-github@users.noreply.github.com>
2021-01-18 15:13:10 -0600

    Add files via upload

---
+++ src/main/java/com/annieliang/covid19/tracker/controllers/HomeController.java
@@ -0,0 +1,33 @@
+package com.annieliang.covid19.tracker.controllers;
+
+import com.annieliang.covid19.tracker.models.LocationStats;
+import com.annieliang.covid19.tracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+/**
+ * @author annieLiang
+ * @create 2021-01-18 2:47 PM
+ */
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+}
